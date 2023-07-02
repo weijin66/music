5db1a2b003845b0d635ad7398d919c0fbce739fe
weijin <1270136538@qq.com>
2023-07-02 21:57:31 +0800

    feat(home): :bug:修改bug
    :bug:修改bug

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -4,7 +4,38 @@
       class="w-[100vw] overflow-hidden bg-[#f7fafc] text-[#000] dark:bg-gray-900 dark:text-[#fff]"
     >
       <!-- 搜索框 -->
-      <search @click.native="shows = !shows"></search>
+      <div class="w-[100vw] flex items-center justify-around mt-3 relative">
+        <Icon
+          icon="mingcute:menu-fill"
+          width="8vw"
+          hight="8vw"
+          @click.native="shows = !shows"
+        />
+        <input
+          type="text"
+          placeholder="张杰"
+          class="w-[72vw] h-[10vw] border-solid border-[0.5vw] rounded-[12vw] border-fuchsia-200 bg-gradient-to-r from-purple-100 to-pink-200 indent-[12vw]"
+        />
+        <Icon
+          icon="circum:search"
+          class="absolute top-[2vw] left-[19vw]"
+          width="6vw"
+          height="6vw"
+          color="gray"
+        />
+        <Icon
+          icon="fluent:scan-qr-code-24-filled"
+          class="absolute top-[1vw] left-[75vw]"
+          width="8vw"
+          height="8vw"
+          color="gray"
+        />
+        <Icon
+          icon="material-symbols:auto-detect-voice-outline"
+          width="8vw"
+          hight="8vw"
+        />
+      </div>
       <van-popup
         v-model="shows"
         position="left"
@@ -405,7 +436,6 @@ export default {
     };
   },
   components: {
-    search: () => import('../../components/Search.vue'),
     swipe: () => import('../../components/Swiper.vue'),
     menulist: () => import('../../components/MenuList.vue'),
     recommendedsonglist: () =>
